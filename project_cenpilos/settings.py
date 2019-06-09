3e622812fa204a1fa303be8649f801b39e8037fc
Zhaoyu Guo <zhaoyu.guo@mail.utoronto.ca>
2019-06-09 09:22:53 -0400

    Updated settings

--- project_cenpilos/settings.py
+++ project_cenpilos/settings.py
@@ -22,7 +22,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'cenpilos', 'static'),
 )
 
 
@@ -135,8 +135,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-
 
 LOGIN_REDIRECT_URL = ''
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
