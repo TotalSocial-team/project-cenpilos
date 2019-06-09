62bcc9bd81af2be9f0b346071f27e1bdf75ba55f
zguo123 <41599103+zguo123@users.noreply.github.com>
2019-06-09 12:26:20 -0400

    Updated settings

--- project_cenpilos/settings.py
+++ project_cenpilos/settings.py
@@ -13,15 +13,16 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.join(BASE_DIR, '../cenpilos')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles/')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
