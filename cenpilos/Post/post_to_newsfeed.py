376cc2fb2489321e9bc7cf4f603461c10d371c36
zguo123 <41599103+zguo123@users.noreply.github.com>
2019-08-19 08:32:57 -0400

    Updated Post Class

--- cenpilos/Post/post_to_newsfeed.py
+++ cenpilos/Post/post_to_newsfeed.py
@@ -1,62 +1,87 @@
-"""
-This class is a part of this program and should NOT under any circumstances be modified
-or deleted!
-
-"""
+from __future__ import annotations
 from cenpilos.models import Posts
 from cenpilos.forms import *
+from datetime import datetime
+from typing import *
+from difflib import SequenceMatcher
 
 
 class Post:
     """
-    Posts the content to the newsfeed
+    A class representing a Post for a specific user.
+
+    === Attributes ===
+    content: the content of the post
+    user_id: the id of the current user.
+    date: the datetime of when the post is posted.
+    comments: a list of comments this post has
+    likes: the number of likes this post has
     """
+    content: Any
+    date: datetime
+    comments: List[Comment]
+    likes: int
+    userid: Any
 
-    def __init__(self, form: PostForm) -> None:
+    def __init__(self, content: Any, user_id: Any) -> None:
+        """ Initializes this class with content as <content>, the <user_id> as the user_id attribute.
+         and date as today's date. Sets the comments attributes to an empty list and likes to be 0.
         """
-        Initializes with a parameter called form
-        :param form: Must be the data from the Post Form
-        """
-        self.post_content = form
-        self.sear_words = list()    # list of all swear words -- initially empty
 
-    def __str__(self) -> str:
-        """
-        Executes when the user types in str(<Post object>) or print(<Post object>)
-        :return: a formatted string containing only the user's post
-        """
-        return str(self.post_content)
+        self.content = content
+        self.date = datetime.today()
+        self.comments = []
+        self.user_id = user_id
+        self.likes = 0
 
-    def __repr__(self) -> str:
-        """
-        Executes when the user types in str(<Post object>) or print(<Post object>)
-        :return: a formatted string containing only the user's post
-        """
-        return str(self.post_content)
+    def update_likes(self, factor: int) -> None:
+        """ Increment the likes attribute by <factor> """
+        self.likes += factor
 
-    def save_post(self):
+    def save(self) -> None:
+        """ Saves the current post into the database. """
+
+        # Saves the post only when there is no swear words.
+        swearwords = self.swearword_filter()
+
+        if not swearwords:
+            ...
+
+    def swearword_filter(self) -> bool:
         """
-        # TODO: Come up with a description for this function :)
-        ASSUMES: the user has a space
-        :return:
+        Returns True iff the percentage of similarity is greater than 50 %.
         """
-        post = [word.split(' ') for word in self.post_content]
 
-        # ADVANCED TECHNIQUE -- BEGINNERS MIGHT NOT UNDERSTAND THIS #
-        def swear_word_checker() -> float:
-            """
-            Uses the post variable created above iterate through to do some tests
-            :return: A percentage out of 100, indicating if the word is a swear word or not
-            """
-            percent = 0.0
-            # TODO: put this section in a filter
-            with open("bad_words.txt", "r") as f:
-                for line in f:
-                    line = line.split(',')      # DO NOT MODIFY bad_words.txt! If you do, this line will NOT WORK!
-                    for i, p in post:
-                        if len(p) == len(line):
-                            if p == line:
-                                percent = 100.0     # FOUND A SWEAR WORD
+        swearwords = self.read_swearwords()
 
-            return percent
+        for swearword in swearwords:
+            for content in self.content:
+                percentage = SequenceMatcher(None, content, swearword).ratio()
 
+                if percentage >= 50.0:
+                    return True
+
+        return False
+
+    @staticmethod
+    def read_swearwords() -> List[Any]:
+        """ Returns a list of all swear words. """
+
+        lst = []
+        with open("bad_words.txt", "r") as f:
+            for line in f:
+                line = line.split()  # DO NOT MODIFY bad_words.txt! If you do, this line will NOT WORK!
+                lst.extend(line)
+
+        return lst
+
+
+class Comment:
+    """ A class representing a comment for a specific post.
+
+    === Attributes ===
+    user_id: the id of the user that made the comment
+    content: the actual content of the comment
+    date: the date at which the post was posted
+
+    """
