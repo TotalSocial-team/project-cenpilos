77975d6b6aa307c39d556c72f6cca0dce20ed2d5
zguo123 <41599103+zguo123@users.noreply.github.com>
2019-08-19 10:20:27 -0400

    Updated Post/Comment classes

--- cenpilos/Post/post_to_newsfeed.py
+++ cenpilos/Post/post_to_newsfeed.py
@@ -45,11 +45,82 @@ class Post:
         swearwords = self.swearword_filter()
 
         if not swearwords:
-            ...
+            # TODO: Implement this function
+            pass
 
     def swearword_filter(self) -> bool:
         """
-        Returns True iff the percentage of similarity is greater than 50 %.
+        Returns True iff the percentage of similarity is greater than 50.0.
+        """
+
+        swearwords = self.read_swearwords()
+
+        for swearword in swearwords:
+            for content in self.content:
+                percentage = SequenceMatcher(None, content, swearword).ratio()
+
+                if percentage >= 50.0:
+                    return True
+
+        return False
+
+    def add_comment(self, content: str) -> None:
+        """ Adds the comment with <content> to the comments attribute """
+
+        self.comments.append(Comment(self.user_id, content))
+
+    def save_comments(self) -> None:
+        """ Saves the comments associated with this post """
+
+        for comment in self.comments:
+            comment.save()
+        
+
+    @staticmethod
+    def read_swearwords() -> List[Any]:
+        """ Returns a list of all swear words. """
+
+        lst = []
+        with open("bad_words.txt", "r") as f:
+            for line in f:
+                line = line.split()  # DO NOT MODIFY bad_words.txt! If you do, this line will NOT WORK!
+                lst.extend(line)
+
+        return lst
+
+
+class Comment:
+    """ A class representing a comment for a specific post.
+
+    === Attributes ===
+    user_id: the id of the user that made the comment
+    content: the actual content of the comment
+    date: the date at which the post was posted
+
+    """
+    user_id: int
+    content: str
+    date: datetime
+
+    def __init__(self, user_id: int, content: str) -> None:
+        """ Initializes the user_id attribute with <user_id> and content with <content> """
+
+        self.user_id = user_id
+        self.content = content
+        self.date = datetime.today()
+
+    def save(self) -> None:
+        """ Saves the current comment """
+
+        is_badword = self.swearword_filter()
+
+        if not is_badword:
+            # TODO: Implement
+            pass
+
+    def swearword_filter(self) -> bool:
+        """
+        Returns True iff the percentage of similarity is greater than 50.0.
         """
 
         swearwords = self.read_swearwords()
@@ -74,14 +145,3 @@ class Post:
                 lst.extend(line)
 
         return lst
-
-
-class Comment:
-    """ A class representing a comment for a specific post.
-
-    === Attributes ===
-    user_id: the id of the user that made the comment
-    content: the actual content of the comment
-    date: the date at which the post was posted
-
-    """
