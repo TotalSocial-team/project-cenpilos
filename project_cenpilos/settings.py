83f6d50d560a2ed462ec4fbfd1011e98b1265cca
zguo123 <41599103+zguo123@users.noreply.github.com>
2019-08-19 20:28:48 -0400

    Fix static file problems.

--- project_cenpilos/settings.py
+++ project_cenpilos/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
