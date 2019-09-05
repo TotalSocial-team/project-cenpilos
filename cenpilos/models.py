ce1fef2b006d540ee5ef296a43867f5f4d78fc40
zguo123 <41599103+zguo123@users.noreply.github.com>
2019-09-05 17:33:39 -0400

    Fixed Post not exist error.

--- cenpilos/models.py
+++ cenpilos/models.py
@@ -3,15 +3,15 @@ from django.db import models
 #
 #
 # # this is where the posts should be saved at
-# class Posts(models.Model):
-#
-#     # associates the user with this post
-#     user_info = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
-#
-#     post_body = models.CharField(max_length=100000)     # user can save up to 1000 characters
-#
-#     # tags are defined in the following way:
-#     #       1. <#><tag_name>
-#     # TODO: Implement truncation logic
-#     # TODO: Create tag fields
-#     # TODO: Change the database type before starting the project
+class Posts(models.Model):
+
+    # associates the user with this post
+    user_info = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+
+    post_body = models.CharField(max_length=100000)     # user can save up to 1000 characters
+
+    # tags are defined in the following way:
+    #       1. <#><tag_name>
+    # TODO: Implement truncation logic
+    # TODO: Create tag fields
+    # TODO: Change the database type before starting the project
