46ceba213c0d403129109c4112bfed32d9905bb5
zguo123 <41599103+zguo123@users.noreply.github.com>
2019-09-05 17:39:41 -0400

    Debug mode turned on

--- project_cenpilos/settings.py
+++ project_cenpilos/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["cenpilos.herokuapp.com", "cenpilos.tech"]
 
